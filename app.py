abb36c4e2d940e35985d73da5ff19323fbc7ced4
Sumit Kumar <31334520+sumitkumarju@users.noreply.github.com>
2020-10-30 16:05:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,47 +16,47 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    temp_array = list()
+    temp = list()
 
 
     if request.method == 'POST':
 
         batting_team = request.form['batting-team']
         if batting_team == 'Chennai Super Kings':
-            temp_array = temp_array + [1,0,0,0,0,0,0,0]
-        elif batting_team == 'Delhi Daredevils':
-            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+            temp = temp + [1,0,0,0,0,0,0,0]
         elif batting_team == 'Kings XI Punjab':
-            temp_array = temp_array + [0,0,1,0,0,0,0,0]
-        elif batting_team == 'Kolkata Knight Riders':
-            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+            temp = temp + [0,0,1,0,0,0,0,0]
+	elif batting_team == 'Delhi Daredevils':
+            temp = temp + [0,1,0,0,0,0,0,0]
         elif batting_team == 'Mumbai Indians':
-            temp_array = temp_array + [0,0,0,0,1,0,0,0]
-        elif batting_team == 'Rajasthan Royals':
-            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+            temp = temp + [0,0,0,0,1,0,0,0]
         elif batting_team == 'Royal Challengers Bangalore':
-            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+            temp = temp + [0,0,0,0,0,0,1,0]
+	elif batting_team == 'Rajasthan Royals':
+            temp = temp + [0,0,0,0,0,1,0,0]
         elif batting_team == 'Sunrisers Hyderabad':
-            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            temp = temp + [0,0,0,0,0,0,0,1]
+	  elif batting_team == 'Kolkata Knight Riders':
+            temp = temp + [0,0,0,1,0,0,0,0]
 
 
         bowling_team = request.form['bowling-team']
         if bowling_team == 'Chennai Super Kings':
-            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+            temp = temp + [1,0,0,0,0,0,0,0]
+	elif bowling_team == 'Rajasthan Royals':
+            temp = temp + [0,0,0,0,0,1,0,0]
         elif bowling_team == 'Delhi Daredevils':
-            temp_array = temp_array + [0,1,0,0,0,0,0,0]
-        elif bowling_team == 'Kings XI Punjab':
-            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+            temp = temp + [0,1,0,0,0,0,0,0]
         elif bowling_team == 'Kolkata Knight Riders':
-            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+            temp = temp + [0,0,0,1,0,0,0,0]
+	 elif bowling_team == 'Sunrisers Hyderabad':
+            temp = temp + [0,0,0,0,0,0,0,1]
         elif bowling_team == 'Mumbai Indians':
-            temp_array = temp_array + [0,0,0,0,1,0,0,0]
-        elif bowling_team == 'Rajasthan Royals':
-            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+            temp = temp + [0,0,0,0,1,0,0,0]
         elif bowling_team == 'Royal Challengers Bangalore':
-            temp_array = temp_array + [0,0,0,0,0,0,1,0]
-        elif bowling_team == 'Sunrisers Hyderabad':
-            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            temp = temp + [0,0,0,0,0,0,1,0]
+	elif bowling_team == 'Kings XI Punjab':
+            temp = temp + [0,0,1,0,0,0,0,0]
 
 
         overs = float(request.form['overs'])
@@ -67,9 +67,9 @@ def predict():
 
 
 
-        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
+        temp = temp + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
 
-        data = np.array([temp_array])
+        data = np.array([temp])
         my_prediction = int(regressor.predict(data)[0])
 
         return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
