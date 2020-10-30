f6007bd90a7f088d3c0b71b6ac357037e34ebb7a
Sumit Kumar <31334520+sumitkumarju@users.noreply.github.com>
2020-10-30 12:26:28 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,19 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
-
-
-# Importing essential libraries
 import pandas as pd
 import pickle
 
-# Loading the dataset
+
 df = pd.read_csv('ipl.csv')
 
 
-# --- Data Cleaning ---
-# Removing unwanted columns
 columns_to_remove = ['mid', 'venue', 'batsman', 'bowler', 'striker', 'non-striker']
 df.drop(labels=columns_to_remove, axis=1, inplace=True)
 
@@ -21,9 +13,6 @@ df.drop(labels=columns_to_remove, axis=1, inplace=True)
 
 df['bat_team'].unique()
 
-
-
-# Keeping only consistent teams
 consistent_teams = ['Kolkata Knight Riders', 'Chennai Super Kings', 'Rajasthan Royals',
                     'Mumbai Indians', 'Kings XI Punjab', 'Royal Challengers Bangalore',
                     'Delhi Daredevils', 'Sunrisers Hyderabad']
@@ -35,9 +24,6 @@ df = df[(df['bat_team'].isin(consistent_teams)) & (df['bowl_team'].isin(consiste
 
 
 
-
-
-# Removing the first 5 overs data in every match
 df = df[df['overs']>=5.0]
 
 
@@ -52,15 +38,12 @@ print(df['bowl_team'].unique())
 
 
 
-# Converting the column 'date' from string into datetime object
 from datetime import datetime
 df['date'] = df['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
 
 
 
 
-# --- Data Preprocessing ---
-# Converting categorical features using OneHotEncoding method
 encoded_df = pd.get_dummies(data=df, columns=['bat_team', 'bowl_team'])
 
 
@@ -76,9 +59,6 @@ encoded_df.columns
 
 
 
-
-
-# Rearranging the columns
 encoded_df = encoded_df[['date', 'bat_team_Chennai Super Kings', 'bat_team_Delhi Daredevils', 'bat_team_Kings XI Punjab',
               'bat_team_Kolkata Knight Riders', 'bat_team_Mumbai Indians', 'bat_team_Rajasthan Royals',
               'bat_team_Royal Challengers Bangalore', 'bat_team_Sunrisers Hyderabad',
@@ -89,9 +69,6 @@ encoded_df = encoded_df[['date', 'bat_team_Chennai Super Kings', 'bat_team_Delhi
 
 
 
-
-
-# Splitting the data into train and test set
 X_train = encoded_df.drop(labels='total', axis=1)[encoded_df['date'].dt.year <= 2016]
 X_test = encoded_df.drop(labels='total', axis=1)[encoded_df['date'].dt.year >= 2017]
 
@@ -107,8 +84,6 @@ y_test = encoded_df[encoded_df['date'].dt.year >= 2017]['total'].values
 
 
 
-
-# Removing the 'date' column
 X_train.drop(labels='date', axis=True, inplace=True)
 X_test.drop(labels='date', axis=True, inplace=True)
 
@@ -119,9 +94,6 @@ X_test.drop(labels='date', axis=True, inplace=True)
 X_train.head()
 
 
-
-# --- Model Building ---
-# Linear Regression Model
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train,y_train)
